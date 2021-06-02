dca1b2510497d5c5dff3d97f75ed5bc894b44ed4
TenDan <tytusworld@gmail.com>
2021-06-02 19:30:41 +0200

    fix vuetify and add dashboard view

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="v-application">
     <!--<div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -21,24 +21,10 @@ export default {
 </script>
 
 <style lang="scss">
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-  }
-
-  #nav {
-    padding: 30px;
-
-    a {
-      font-weight: bold;
-      color: #2c3e50;
-
-      &.router-link-exact-active {
-        color: #42b983;
-      }
-    }
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    border: 0;
   }
 </style>
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -2,19 +2,25 @@
   <v-card
     max-width="100%"
     height="100vh"
-    class="mx-auto"
   >
-    <v-navigation-drawer permanent>
-      <v-list-item class="text-start">
-        <v-list-item-content class="text-left">
-          <v-list-item-title class="text-h6">
-            Imię Nazwisko
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            adres e-mail
-          </v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
+    <v-navigation-drawer permanent expand-on-hover>
+      <v-list>
+        <v-list-item class="px-2">
+          <v-list-item-avatar>
+            <v-avatar color="primary" size="56">IN</v-avatar>
+          </v-list-item-avatar>
+        </v-list-item>
+        <v-list-item class="text-start">
+          <v-list-item-content class="text-left">
+            <v-list-item-title class="text-h6">
+              Imię Nazwisko
+            </v-list-item-title>
+            <v-list-item-subtitle>
+              nazwa użytkownika
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
       <v-divider></v-divider>
       <v-list
         dense
@@ -24,6 +30,7 @@
                      :key="item.title"
                      link
                      :to="item.link"
+                     class="text-center"
         >
           <v-list-item-icon>
             <v-icon>{{item.icon}}</v-icon>
@@ -33,6 +40,18 @@
           </v-list-item-content>
         </v-list-item>
       </v-list>
+      <template v-slot:append>
+        <v-list dense nav>
+          <v-list-item class="text-center" link>
+            <v-list-item-icon>
+              <v-icon>logout</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>Wyloguj się</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </template>
     </v-navigation-drawer>
   </v-card>
 </template>
@@ -46,7 +65,7 @@ export default {
         {
           title: 'Panel główny',
           icon: 'dashboard',
-          link: '/',
+          link: '/dashboard',
         },
         {
           title: 'Ustawienia',
