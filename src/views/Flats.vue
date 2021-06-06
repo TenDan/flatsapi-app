f46fd3849cf4f02197da4c06af9ee14bd08b3760
TenDan <tytusworld@gmail.com>
2021-06-06 21:22:21 +0200

    flats section expand

--- src/views/Flats.vue
+++ src/views/Flats.vue
@@ -1,7 +1,39 @@
 <template>
-  <v-card>
-    <v-card-title>Flats</v-card-title>
-  </v-card>
+  <v-content class="pa-5">
+    <v-card elevation="5" class="mx-5 my-8" v-for="n in 10" :key="n">
+      <v-row justify="space-between" align="center">
+        <v-col cols="6">
+          <v-card-title class="text-h3">Mieszkanie nr {{n}}</v-card-title>
+        </v-col>
+        <v-col cols="3">
+          <div class="text-h3 text-center">{{n * 10000}} zł</div>
+        </v-col>
+      </v-row>
+      <v-card-text class="text-body-1">Lorem ipsum</v-card-text>
+      <v-card-actions>
+        <v-btn icon>
+          <v-icon>info</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>shopping_bag</v-icon>
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+    <v-fab-transition>
+      <v-btn
+        elevation="2"
+        fab
+        bottom
+        large
+        right
+        color="primary"
+        fixed
+        class="ma-5"
+      >
+        <v-icon>add</v-icon>
+      </v-btn>
+    </v-fab-transition>
+  </v-content>
 </template>
 
 <script>
