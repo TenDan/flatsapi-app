68d6d51c88097599ae59032435c349bcf1bb4f1c
TenDan <tytusworld@gmail.com>
2021-06-11 21:44:43 +0200

    add page titles to routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,31 +4,48 @@ import Home from '../views/Home.vue';
 
 Vue.use(VueRouter);
 
+const PAGE_TITLE = 'Flat of Blocks';
+
 const routes: Array<RouteConfig> = [
   {
     path: '/',
     name: 'Home',
     component: Home,
+    meta: {
+      title: `Strona główna - ${PAGE_TITLE}`
+    }
   },
   {
     path: '/dashboard',
     name: 'Dashboard',
     component: () => import('../views/Dashboard.vue'),
+    meta: {
+      title: `Panel główny - ${PAGE_TITLE}`
+    }
   },
   {
     path: '/flats',
     name: 'Flats',
     component: () => import('../views/Flats.vue'),
+    meta: {
+      title: `Mieszkania - ${PAGE_TITLE}`
+    }
   },
   {
     path: '/settings',
     name: 'Settings',
     component: () => import('../views/Settings.vue'),
+    meta: {
+      title: `Ustawienia - ${PAGE_TITLE}`
+    }
   },
   {
     path: '/tenants',
     name: 'Tenants',
     component: () => import('../views/Tenants.vue'),
+    meta: {
+      title: `Lokatorzy - ${PAGE_TITLE}`
+    }
   },
 ];
 
