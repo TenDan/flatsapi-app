daafa1e88a2e0c533c0875613f0e261c3d3dc6c2
TenDan <tytusworld@gmail.com>
2021-07-17 13:38:51 +0200

    Add styles and HTML to Home component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,48 @@
 <template>
-  <div></div>
+  <div class="frontPage">
+    <v-card color="transparent" elevation="0">
+      <v-card-title class="flex justify-center my-2">
+        <div class="text-h1 text-center wrap">
+          Flats Of Blocks
+        </div>
+      </v-card-title>
+      <v-card-subtitle class="text-center wrap text-h5">
+        Manage your flats and blocks of flats in one place.
+      </v-card-subtitle>
+      <v-card-actions class="flex justify-center">
+        <v-btn outlined>
+          <v-icon
+            left>
+            person
+          </v-icon>
+          Login
+        </v-btn>
+        <v-btn outlined>
+          <v-icon
+            left>
+            person_add
+          </v-icon>
+          Register
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'Home',
-  /* components: {
-    HelloWorld,
-  }, */
+
 };
 </script>
+
+<style>
+  .frontPage {
+    position: absolute;
+    width: 100%;
+    top: 50%;
+    transform: translateY(-50%);
+    word-wrap: break-spaces;
+  }
+</style>
