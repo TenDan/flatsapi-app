8638dcdba90e5f8258d78dc385112c8e011d589f
TenDan <tytusworld@gmail.com>
2021-07-21 13:54:46 +0200

    Fix the undefined reference problem

--- src/common/form/rules.ts
+++ src/common/form/rules.ts
@@ -1,2 +1,4 @@
 export const required = (value: any) => !!value || 'Pole wymagane';
 export const mustBeANumber = (value: any) => !!(+value) || 'To musi być liczba';
+export const minLength = (length: number) => (value: any) => value?.length >= length || `Wartość jest zbyt krótka. Co najmniej: ${length} znaków`;
+export const maxLength = (length: number) => (value: any) => value?.length < length || `Wartość jest zbyt długa. Co najwyżej: ${length} znaków`;
