fe78603495e0d3b4dfc3e61e32aabb27a1ed6dbc
TenDan <tytusworld@gmail.com>
2021-07-23 20:54:57 +0200

    Add isEmail method to check if specified string matches email regex

--- src/common/form/rules.ts
+++ src/common/form/rules.ts
@@ -1,4 +1,8 @@
+const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//return re.test(String(email).toLowerCase());
+
 export const required = (value: any) => !!value || 'Pole wymagane';
 export const mustBeANumber = (value: any) => !!(+value) || 'To musi być liczba';
 export const minLength = (length: number) => (value: any) => value?.length >= length || `Wartość jest zbyt krótka. Co najmniej: ${length} znaków`;
 export const maxLength = (length: number) => (value: any) => value?.length < length || `Wartość jest zbyt długa. Co najwyżej: ${length} znaków`;
+export const isEmail = (value: string) => emailRegex.test(value.toLowerCase()) || 'To musi być adres e-mail';
